01e4522023b0146ca1768a9638644b19a3ef5aed
Matthias Christen <christen@vanamco.com>
2017-12-01 22:56:41 +0100

    HTC-0001: added diff tests

---
+++ tests/difftests/_scripts/test.js
@@ -0,0 +1,46 @@
+document.write('<script type="text/javascript" src="_scripts/jquery-3.2.1.min.js"></script>');
+document.write('<script type="text/javascript" src="_scripts/bluebird.js"></script>');
+document.write('<script type="text/javascript" src="_scripts/specificity.js"></script>');
+document.write('<script type="text/javascript" src="../../dist/html2canvas.js"></script>');
+document.write('<script type="text/javascript" src="_scripts/fabric.js"></script>');
+
+setTimeout(function()
+{
+	$(document).ready(function()
+	{
+		if (window.setUp)
+	        window.setUp();
+	});	
+}, 100);
+
+setTimeout(function()
+{
+	html2canvas(
+		document.documentElement,
+		{
+			backgroundColor: '#ffffff',
+			type: 'view',
+			scale: 1,
+			letterRendering: true,
+			javascriptEnabled: true,
+			proxy: 'areion'
+		}
+	).then(function(canvas)
+	{
+		var len = document.body.childNodes.length;
+		for (var i = 0; i < len; i++)
+		{
+			var node = document.body.childNodes[i];
+			if (node.nodeType === 1)
+				node.style.display = 'none';
+		}
+
+		canvas.style.position = 'absolute';
+		canvas.style.left = '0';
+		canvas.style.top = '0';
+		canvas.style.padding = '0';
+		canvas.style.opacity = '1';
+
+		document.body.append(canvas);
+	});
+}, 2000);
